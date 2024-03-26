322f97d29f9469b55ffd358ba97865c929e430c3
Carlo Pinto <carlo.pinto@calytrix.com>
2024-03-26 16:31:58 +0000

    Init setup #1

--- llmgame/__init__.py
+++ llmgame/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from . import db
 from . import llmgame
 
 
@@ -8,13 +9,13 @@ def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
 
-    # db_path = os.path.join(app.instance_path, 'blog.sqlite')
+    db_path = os.path.join(app.instance_path, 'game.sqlite')
 
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY='dev',
-        # # store the database in the instance folder
-        # DATABASE=db_path,
+        # store the database in the instance folder
+        DATABASE=db_path,
     )
 
     # if test_config is None:
@@ -31,13 +32,13 @@ def create_app(test_config=None):
         pass
 
     # check if database exists, if not initialise it
-    # if not os.path.isfile(db_path): # pragma: no cover
-    #     with app.app_context():
-    #         db.init_db()
-    #         print("Initialized the database.")
+    if not os.path.isfile(db_path): # pragma: no cover
+        with app.app_context():
+            db.init_db()
+            print("Initialized the database.")
 
     # register the database commands
-    # db.init_app(app)
+    db.init_app(app)
 
     # apply the blueprints to the app
     app.register_blueprint(llmgame.bp)
